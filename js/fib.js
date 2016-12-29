3501f3328fdce09644d9c22fddbcacd2fb5caa07
pattikawa <pattikawa@yahoo.com>
2016-12-29 18:46:23 +0100

    First commit

---
+++ js/fib.js
@@ -0,0 +1,7 @@
+<script>
+document.write("<h2> Table of Fibonacci nummers <h2>");
+for(i=0, j=1, k=0, f=0; i<100; i++, f=j+k,j=k, k=f {
+	document.write("Fibonacci(" + i + ")=" +f);
+	document.write("<br>");
+})
+</script>
